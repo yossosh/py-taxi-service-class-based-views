e81e1e704ec6e43e45b7f838d9406a065afb273b
Sofiia <sonagm9@gmail.com>
2024-05-10 23:44:44 +0300

    Solution fixed flake8

--- taxi/migrations/0002_alter_car_options_alter_driver_options_and_more.py
+++ taxi/migrations/0002_alter_car_options_alter_driver_options_and_more.py
@@ -6,20 +6,20 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('taxi', '0001_initial'),
+        ("taxi", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='car',
-            options={'ordering': ['model']},
+            name="car",
+            options={"ordering": ["model"]},
         ),
         migrations.AlterModelOptions(
-            name='driver',
-            options={'ordering': ['username']},
+            name="driver",
+            options={"ordering": ["username"]},
         ),
         migrations.AlterModelOptions(
-            name='manufacturer',
-            options={'ordering': ['name']},
+            name="manufacturer",
+            options={"ordering": ["name"]},
         ),
     ]
--- taxi/urls.py
+++ taxi/urls.py
@@ -6,25 +6,30 @@ from .views import (
     CarListView,
     CarDetailView,
     DriverListView,
-    DriverDetailView
+    DriverDetailView,
 )
 
 urlpatterns = [
     path("",
-        index,
-        name="index"),
+         index,
+         name="index"),
+
     path("manufacturers/",
          ManufacturerListView.as_view(),
          name="manufacturer-list"),
+
     path("cars/",
          CarListView.as_view(),
          name="car-list"),
+
     path("cars/<int:pk>/",
          CarDetailView.as_view(),
          name="car-detail"),
+
     path("drivers/",
          DriverListView.as_view(),
          name="driver-list"),
+
     path("drivers/<int:pk>/",
          DriverDetailView.as_view(),
          name="driver-detail"),
